0e3ba737d498476b986c6fd83b330739f9cd46a9
Larry-lai-smarthon <larry.lai.smarthon@gmail.com>
2025-07-24 15:41:23 +0800

    add the water quality uint

--- exsmartcity.ts
+++ exsmartcity.ts
@@ -342,7 +342,7 @@ namespace SmartCityExtension {
     //% weight=80
     //% group="TDS Value"
     //% blockId="smarthon_read_TDC_value"
-    //% block="Read water quality at Pin %pin"
+    //% block="Read water quality(ppm) at Pin %pin"
     export function getTDSValue(pin: AnalogPin): number {
         return readTDSValue(pin);
     }
